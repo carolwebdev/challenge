9a385767a09a28de230a91a31ed42df7bb92424f
Ana Carolina de Araujo <79064264+carolwebdev@users.noreply.github.com>
2022-03-02 20:34:56 -0300

    Update script.js
    Adding hourglass upsidedown.

--- script.js
+++ script.js
@@ -5,28 +5,10 @@ var leitor = readline.createInterface({
     output: process.stdout
 });
 
-leitor.question("Digite um valor maior ou igual a 20.\n", function(size) {
+leitor.question("Digite um valor maior ou igual a 20.\n", function (size) {
 
-    function hourglassOddNumber(size) {
-    console.log('#'.repeat(size));
-    for (let index = 1; (index) < (size / 2) - 1; index++) {
-        console.log(
-            '#' + ' '.repeat(index - 1) + '#' + ' '.repeat(size - (index * 2) - 2) + '#' + ' '.repeat(index - 1) + "#"
-        );
-    }
-
-    console.log('#' + ' '.repeat((size / 2) - 1) + "#" + ' '.repeat((size / 2) - 1) + '#')
-    console.log('#' + ' '.repeat((size / 2) - 1) + "##" + ' '.repeat((size / 2) - 2) + '#')
-
-    for (let index = ((size/2)+1); (index) < (size -2); index++) {
-        console.log(
-            '#' + " ".repeat((size) - (index) -1) + '#' + '#'.repeat(-size - (index * -2)) + '#' + " ".repeat((size) - (index) -2) + "#"
-        );
-    }
-    console.log('#'.repeat((size)));
-}
-
-    function hourglassEvenNumber(size) {
+    function hourGlassOddNumberUp(size) {
+        size++;
         console.log('#'.repeat(size));
         for (let index = 1; (index) < (size / 2) - 1; index++) {
             console.log(
@@ -35,23 +17,86 @@ leitor.question("Digite um valor maior ou igual a 20.\n", function(size) {
         }
 
         console.log('#' + ' '.repeat((size / 2) - 2) + "##" + ' '.repeat((size / 2) - 2) + '#')
-        console.log('#' + ' '.repeat((size / 2) - 2) + "##" + ' '.repeat((size / 2) - 2) + '#')
 
-        for (let index = ((size/2)+1); (index) < (size -1); index++) {
+        for (let index = ((size / 2) + 1); (index) < (size - 2); index++) {
             console.log(
-                '#' + " ".repeat((size) - (index) -2) + '#' + '#'.repeat(-size - (index * -2)) + '#' + " ".repeat((size) - (index) -2) + "#"
+                '#' + " ".repeat((size) - (index) - 2) + '#' + '#'.repeat(-size - (index * -2)) + '#' + " ".repeat((size) - (index) - 2) + "#"
             );
         }
-        console.log('#'.repeat((size)));
+        console.log('#'.repeat((size))+'\n');
     }
 
-    if (size%2 == 0 && size >=20){
-        hourglassEvenNumber(size);
+    function hourglassEvenNumberUp(size) {
+        size++;
+        console.log('#'.repeat(size));
+        for (let index = 1; (index) < ((size / 2) - 1); index++) {
+            console.log(
+                '#' + ' '.repeat(index - 1) + '#' + ' '.repeat(size - (index * 2) - 2) + '#' + ' '.repeat(index - 1) + "#"
+            );
+        }
+
+        console.log('#' + ' '.repeat((size / 2) - 1) + "#" + ' '.repeat((size / 2) - 1) + '#')
+
+
+        for (let index = ((size / 2)); (index) < (size - 2); index++) {
+            console.log(
+                '#' + " ".repeat((size) - (index) - 2) + '#' + '#'.repeat(-size - ((index * -2) - 1)) + '#' + " ".repeat((size) - (index) - 2) + "#"
+            );
+        }
+        console.log('#'.repeat((size))+'\n');
     }
-    if (size%2 != 0 && size >=20){
-        hourglassOddNumber(size);
+
+    function hourglassOddNumberDown(size) {
+        size++;
+        console.log('#'.repeat(size));
+        for (let index = 1; (index) < (size / 2) - 1; index++) {
+            console.log(
+                '#' + ' '.repeat(index - 1) + ' ' + '#'.repeat(size - (index * 2) - 2) + ' ' + ' '.repeat(index - 1) + "#"
+            );
+        }
+
+        console.log('#' + ' '.repeat((size / 2) - 2) + "##" + ' '.repeat((size / 2) - 2) + '#')
+
+        for (let index = ((size / 2) + 1); (index) < (size - 2); index++) {
+            console.log(
+                '#' + ' '.repeat((size) - (index) - 2) + '#' + ' '.repeat(-size - (index * -2)) + '#' + ' '.repeat((size) - (index) - 2) + "#"
+            );
+        }
+        console.log('#'.repeat((size))+'\n');
     }
-    if (size < 20){
+
+    function hourglassEvenNumberDown(size) {
+        size++;
+        console.log('#'.repeat(size));
+        for (let index = 1; (index) < ((size / 2) - 1); index++) {
+            console.log(
+                '#' + ' '.repeat(index - 1) + ' ' + '#'.repeat(size - (index * 2) - 2) + ' ' + ' '.repeat(index - 1) + "#"
+            );
+        }
+
+        console.log('#' + ' '.repeat((size / 2) - 1) + "#" + ' '.repeat((size / 2) - 1) + '#')
+
+
+        for (let index = ((size / 2)); (index) < (size - 2); index++) {
+            console.log(
+                '#' + ' '.repeat((size) - (index) - 2) + '#' + ' '.repeat(-size - ((index * -2) - 1)) + '#' + ' '.repeat((size) - (index) - 2) + "#"
+            );
+        }
+        console.log('#'.repeat((size))+'\n');
+    }
+
+
+    if (size % 2 == 0 && size >= 20) {
+        hourglassEvenNumberDown(size);
+        hourglassEvenNumberUp(size);
+
+    }
+    if (size % 2 != 0 && size >= 20) {
+        hourglassOddNumberDown(size);
+        hourGlassOddNumberUp(size);
+
+    }
+    if (size < 20) {
         console.log("O número digitado é menor do que vinte. Digite um número maior ou igual a vinte.")
     }
-    });
+});
